d4ec3df9b7981f1565170d2857211ff9396d0f91
GIULIA JOHANSSON RODRIGUES <giulia_rodrigues@estudante.sesisenai.org.br>
2024-05-06 09:37:43 -0300

    revisao

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './style/style.css'
+import FlexBox from './components/Flexbox.jsx'
+
+function App() {
+  return (
+    <FlexBox/>
+  )
+}
+
+export default App
---
+++ src/components/Flexbox.jsx
@@ -0,0 +1,22 @@
+import '../style/flexbox.css'
+
+function FlexBox() {
+  
+    return (
+        <section>
+            <div className="item1">
+                Item 1
+            </div>
+
+            <div className="item2">
+                Item 2
+            </div>
+
+            <div className="item3">
+                Item 3
+            </div>
+        </section>
+    )
+  }
+  
+  export default FlexBox
